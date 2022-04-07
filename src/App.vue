4b66168104d94bd7aeb7038a364700086f9c0eea
furia0928 <furia0928@gmail.com>
2022-04-07 21:30:24 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,52 @@
 </template>
 
 <style></style>
+<script>
+export default {
+  name: "App",
+  metaInfo() {
+    return {
+      title: "app",
+      titleTemplate: "test | %s",
+      meta: [
+        {
+          name: "title",
+          content: "hana-test",
+        },
+        {
+          name: "description",
+          content: "home",
+        },
+        {
+          name: "keywords",
+          content: "",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:title",
+          content: "hana-test",
+          vmid: "og:title",
+        },
+        {
+          property: "og:description",
+          content: "",
+          vmid: "og:description",
+        },
+        {
+          property: "og:image",
+          content: process.env.VUE_APP_URL + "images/share1.png",
+          vmid: "og:image",
+        },
+        {
+          property: "og:url",
+          content: process.env.VUE_APP_URL,
+          vmid: "og:url",
+        },
+      ],
+    };
+  },
+};
+</script>
