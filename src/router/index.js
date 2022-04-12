b74127b7dff44c16686f2e36e58d080b441dcd1b
furia0928 <furia0928@gmail.com>
2022-04-12 14:53:31 +0900

    init

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: "/about/:id",
+    path: "/about-view",
     name: "About View",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/about_view.vue"),
